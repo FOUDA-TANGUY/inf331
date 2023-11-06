cc215583c6374800ee5912fecdb902e994e38ee9
TANGUY <blondelle.fouda@facsciences-uy1.cm>
2023-11-06 11:10:05 +0100

    Creation de la classe point et ses methode

--- mypackage/Point.java
+++ mypackage/Point.java
@@ -1,1 +1,22 @@
 package mypackage;
+
+public class Point{
+
+    private int abs;
+    private int ord;
+    
+    public void initialiser(int abs, int ord){
+        this.abs = abs;
+        this.ord = ord;
+    }
+
+    public void deplacer(int x, int y){
+        this.abs += x;
+        this.ord += y;
+    }
+
+    public void affiche(){
+        System.out.println("Je suis un point de coordonnees "+this.abs + " et " + this.ord);
+    }
+
+}
